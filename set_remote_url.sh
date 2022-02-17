aa9cb965d446fd327e59cd8c89a08b5cbf75e7b9
Alan Wu <seagarwu@gmail.com>
2022-02-17 22:44:24 +0800

    modify remote url

--- set_remote_url.sh
+++ set_remote_url.sh
@@ -3,7 +3,7 @@ git branch -M main
 git remote rm github
 git remote rm bit
 git remote rm gdrive
-git remote add github git@github.com:seagarwu/End-to-End-Machine-Learning-Pipeline-With-MLOps-Tools.git
-git remote add gdrive http://localhost:3000/seagarwu/End-to-End-Machine-Learning-Pipeline-With-MLOps-Tools.git
-git remote add bit git@bitbucket.org:seagarwu/End-to-End-Machine-Learning-Pipeline-With-MLOps-Tools.git
+git remote add github git@github.com:seagarwu/end-to-end-mlops-pipeline.git
+git remote add gdrive http://localhost:3000/seagarwu/end-to-end-mlops-pipeline.git
+git remote add bit git@bitbucket.org:seagarwu/end-to-end-mlops-pipeline.git
 git remote -v
